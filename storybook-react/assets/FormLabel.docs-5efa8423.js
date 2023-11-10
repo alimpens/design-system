d31b0860e3f5c4d7641ca76d1f0856c9dde61c72
alimpens <alimpens@users.noreply.github.com>
2023-11-10 13:57:27 +0000

    Deploying to gh-pages from @ alimpens/design-system@56da1f9af8d145a89c02b0442a1a1c4298ee9940 🚀

---
+++ storybook-react/assets/FormLabel.docs-5efa8423.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index.esm-beb97e0d.js";import{M as o,d as s,P as m,e as a}from"./index-a5ce3a1d.js";import{F as i}from"./FormLabel.stories-87bad39b.js";import{u as l}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-a4edf1e3.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const p="# Form Label\n\nEen label element gebruik je om een formulierelement te beschrijven. Koppel een formulierelement (zoals een `input` of `textarea`) altijd aan een label.\n";function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:i}),`
+`,e.jsx(s,{children:p}),`
+`,e.jsx(m,{}),`
+`,e.jsx(a,{})]})}function E(t={}){const{wrapper:n}=Object.assign({},l(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(r,t)})):r()}export{E as default};
+//# sourceMappingURL=FormLabel.docs-5efa8423.js.map
--- test-2/assets/preview-1cfd46ee.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-5e30bdfc.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-b6fb8590.js"),["./DocsRenderer-NNNQARDV-b6fb8590.js","./iframe-5e30bdfc.js","./index-565f84a4.js","./_commonjsHelpers-de833af9.js","./react-18-e033092d.js","./index-f7a55a81.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-1cfd46ee.js.map
