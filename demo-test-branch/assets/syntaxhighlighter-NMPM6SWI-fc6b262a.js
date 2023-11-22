5e35813e8eadff1ac8072b1f60cd839a3155529b
alimpens <runner@fv-az1052-447.5caifld0q3felhmjtokckffv3f.phxx.internal.cloudapp.net>
2023-11-22 12:56:04 +0000

    Remove feature branch folder

--- demo-test-branch/assets/syntaxhighlighter-NMPM6SWI-fc6b262a.js
+++
@@ -1,2 +0,0 @@
-import{S as s,c,s as l}from"./index-f454abc7.js";import"./iframe-9d380023.js";import"../sb-preview/runtime.js";import"./index-565f84a4.js";import"./_commonjsHelpers-de833af9.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{s as SyntaxHighlighter,c as createCopyToClipboardFunction,l as default};
-//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-fc6b262a.js.map
