afb53fc56a198a3d92691a6d94c6c898b4089646
alimpens <runner@fv-az740-82.ps3gqx3xffzu3a3pyfxfsa1ajb.bx.internal.cloudapp.net>
2023-11-22 10:49:18 +0000

    Remove feature branch folder

--- demo-test-pr-validation/assets/preview-e12c49a4.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-9d380023.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-4f5ecdd3.js"),["./DocsRenderer-NNNQARDV-4f5ecdd3.js","./iframe-9d380023.js","./index-565f84a4.js","./_commonjsHelpers-de833af9.js","./react-18-e033092d.js","./index-f454abc7.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-e12c49a4.js.map
