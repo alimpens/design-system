72b89aa2d09cd4d26d8ee84f75c44d6e2d35bbd0
alimpens <37216945+alimpens@users.noreply.github.com>
2023-12-13 13:51:12 +0000

    Deploying to gh-pages from @ alimpens/design-system@2dfa5df251644c0262ec5fb3c7537144c8f7ab4e 🚀

---
+++ demo-develop/storybook-react/assets/preview-b5bf8721.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-4083b0ea.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-9a6b4f11.js"),["./DocsRenderer-NNNQARDV-9a6b4f11.js","./iframe-4083b0ea.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-a1e584fd.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-b5bf8721.js.map
