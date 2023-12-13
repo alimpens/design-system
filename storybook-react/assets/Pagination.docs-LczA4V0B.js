f7cd5e9aa7d11835a2afc7bc792cf42ca5092152
alimpens <alimpens@users.noreply.github.com>
2023-12-13 12:37:43 +0000

    Deploying to gh-pages from @ alimpens/design-system@4e2ba271a6d29453c73566b299ef36d2938b2354 🚀

---
+++ storybook-react/assets/Pagination.docs-LczA4V0B.js
@@ -0,0 +1,23 @@
+import{j as e}from"./index.esm-PbY4dSww.js";import{M as i,d as r,P as o,e as s}from"./index-E7igw3OC.js";import{P as g}from"./Pagination.stories-Ko9EBzjL.js";import{u as d}from"./index-Dbo06S9W.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-6vvEBbIL.js";import"../sb-preview/runtime.js";import"./index-f2OjG03D.js";import"./index-aKxH7ZY0.js";import"./index-PPLHz8o0.js";const l=`# Pagination
+
+Pagination (in het Nederlands: paginering) is een navigatie-element onder een zoekresultatenlijst. Bij grote hoeveelheden zoekresultaten kan het duidelijker of functioneler zijn om de inhoud over meerdere pagina´s te verdelen. Paginering toont op welke zoekresultatenlijst de gebruiker zich bevindt en kan hiermee naar een andere zoekresultatenlijst navigeren.
+
+## Richtlijnen
+
+- Gebruik paginering alleen op een zoekresultatenpagina.
+- Voeg de paginering toe na de lijst met zoekresultaten.
+- Start een zoekresultatenpagina bovenaan de pagina na het veranderen van pagina.
+- De paginering kan gecombineerd worden met een teller bovenaan de pagina die “Pagina # van ##” aanduidt.
+- De paginering wordt niet getoond als er maar 1 pagina is.
+- Verwijs de gebruikers door naar de eerste pagina als ze een URL opgeven van een paginanummer dat niet (meer) bestaat.
+
+## Relevante WCAG regels
+
+- [WCAG 2.4.8](https://www.w3.org/TR/WCAG22/#location): geef aan waar de gebruiker is in een verzameling van pagina's (AAA).
+
+Pagination is een interactief element, hier gelden [de algemene eisen en richtlijnen voor interactieve elementen](https://amsterdam.github.io/design-system/?path=/docs/docs-designrichtlijnen-interactieve-elementen--docs) voor.
+`;function t(n){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:g}),`
+`,e.jsx(r,{children:l}),`
+`,e.jsx(o,{}),`
+`,e.jsx(s,{})]})}function z(n={}){const{wrapper:a}=Object.assign({},d(),n.components);return a?e.jsx(a,Object.assign({},n,{children:e.jsx(t,n)})):t()}export{z as default};
+//# sourceMappingURL=Pagination.docs-LczA4V0B.js.map
