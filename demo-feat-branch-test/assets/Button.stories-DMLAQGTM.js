c5c07424f40ec9c629a93ca8ca795576f8bf93c8
alimpens <37216945+alimpens@users.noreply.github.com>
2024-10-30 14:33:56 +0000

    Deploying to gh-pages from @ alimpens/design-system@4557f9ef7d9cf1e3316535f0195c0040793cacdc 🚀

---
+++ demo-feat-branch-test/assets/Button.stories-DMLAQGTM.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{S as B}from"./index.esm-CahSCGl1.js";import{B as T}from"./Button-DuJ28c_X.js";import{I as j}from"./Icon-Zdc7Q_73.js";const W={title:"Components/Buttons/Button",component:T,args:{children:"Button label",variant:"primary",disabled:!1},argTypes:{children:{table:{disable:!1}}}},r={},e={args:{variant:"secondary"}},a={args:{variant:"tertiary"}},s={args:{children:["Button label",o.jsx(j,{svg:B,size:"level-5"},"icon")]},argTypes:{children:{table:{disable:!0}}}},t={args:{children:"Vergunningsaanvraag verzenden"},decorators:[f=>o.jsx("div",{style:{maxWidth:"16rem"},children:o.jsx(f,{})})]};var n,c,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var d,l,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(m=(l=e.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var p,u,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: 'tertiary'
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var y,v,S;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    children: ['Button label', <Icon key="icon" svg={ShareIcon} size="level-5" />]
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(S=(v=s.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var h,b,x;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: 'Vergunningsaanvraag verzenden'
+  },
+  decorators: [Story => <div style={{
+    maxWidth: '16rem'
+  }}>
+        <Story />
+      </div>]
+}`,...(x=(b=t.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};const I=["Primary","Secondary","Tertiary","WithIcon","TextWrapping"],E=Object.freeze(Object.defineProperty({__proto__:null,Primary:r,Secondary:e,Tertiary:a,TextWrapping:t,WithIcon:s,__namedExportsOrder:I,default:W},Symbol.toStringTag,{value:"Module"}));export{E as B,r as P,e as S,a as T,s as W,t as a};
---
+++ demo-feat-branch-test/assets/Pagination.docs-CBK98cXJ.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as a}from"./index-BI1Biiay.js";import{ae as o,ak as s,al as r,am as l}from"./index-BBX-Rjon.js";import{P as p}from"./Pagination.stories-CUxRizVz.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BaIvdoLi.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-CahSCGl1.js";import"./clsx-B-dksMZM.js";import"./Icon-Zdc7Q_73.js";const g=`<!-- @license CC0-1.0 -->
+
+# Pagination
+
+Pagination is a navigation element used to navigate between multiple pages of overview lists.
+This is commonly used for search results or a list of news articles.
+In cases where overview lists are too long to display on a single page, pagination can provide a clearer and more functional way to divide the content across multiple pages.
+Pagination indicates the current page and allows users to navigate to other pages.
+
+## Guidelines
+
+- Use pagination only when an overview list is too long to display on a single page.
+- Place pagination below the overview list.
+- Pagination should not be displayed if there is only one page.
+- Redirect users to the first page if they enter a URL with a page number that doesn’t exist.
+- Pagination can be combined with a counter at the top of the page indicating “Page # of ##.”
+- Start a page with an overview list at the top after changing the page.
+
+## Relevant WCAG Rules
+
+Pagination is an interactive element, and the [general requirements and guidelines for interactive elements](/docs/docs-design-guidelines-interactive-elements--docs) apply.
+`;function i(n){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:p}),`
+`,e.jsx(s,{children:g}),`
+`,e.jsx(r,{}),`
+`,e.jsx(l,{})]})}function R(n={}){const{wrapper:t}={...a(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i()}export{R as default};
