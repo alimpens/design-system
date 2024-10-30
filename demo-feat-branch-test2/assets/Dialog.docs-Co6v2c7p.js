fe647f4ffa73f34d1e8c26298c4dc642e0797862
alimpens <37216945+alimpens@users.noreply.github.com>
2024-10-30 14:41:38 +0000

    Deploying to gh-pages from @ alimpens/design-system@3e98e0fe184a1866eb66d24d44846c02f110cb58 🚀

---
+++ demo-feat-branch-test2/assets/Dialog.docs-Co6v2c7p.js
@@ -0,0 +1,36 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as a}from"./index-BI1Biiay.js";import{ae as i,ak as r,al as l,am as m,an as e}from"./index-BBX-Rjon.js";import{D as p,W as d,T as c}from"./Dialog.stories-BP3DWVzJ.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BaIvdoLi.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Heading-gp4xEVu7.js";import"./getHeadingElement-DiRwFEz7.js";import"./IconButton-DQ5BxJTA.js";import"./index.esm-CahSCGl1.js";import"./VisuallyHidden-hiyECSa5.js";import"./Icon-Zdc7Q_73.js";import"./Paragraph-uZfWSMvY.js";import"./Button-DuJ28c_X.js";const h=`<!-- @license CC0-1.0 -->
+
+# Dialog
+
+A Dialog allows the user to focus on one task or a piece of information by popping up and blocking the page content until the modal task is completed or until the user dismisses the action.
+
+## Guidelines
+
+- Use dialogs sparingly because they interrupt the user’s workflow.
+- Use a dialog for short and non-frequent tasks.
+  Consider using the main flow for regular tasks.
+
+## Keyboard Support
+
+| key         | function                                                         |
+| :---------- | :--------------------------------------------------------------- |
+| Tab         | Moves focus to the next focusable element inside the dialog.     |
+| Shift + Tab | Moves focus to the previous focusable element inside the dialog. |
+| Escape      | Closes the dialog.                                               |
+
+## References
+
+- [HTMLDialogElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement)
+- [Return value](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/returnValue)
+- [Modal & Nonmodal Dialogs: When (& When Not) to Use Them](https://www.nngroup.com/articles/modal-nonmodal-dialog/)
+- [Patterns](https://www.w3.org/WAI/ARIA/apg/patterns/dialog-modal/)
+`;function s(t){const n={h2:"h2",p:"p",...a(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:p}),`
+`,o.jsx(r,{children:h}),`
+`,o.jsx(l,{}),`
+`,o.jsx(m,{}),`
+`,o.jsx(n.h2,{id:"with-scrollbar",children:"With Scrollbar"}),`
+`,o.jsx(n.p,{children:"Content taller than the dialog itself will scroll."}),`
+`,o.jsx(e,{of:d,className:"ams-dialog-story"}),`
+`,o.jsx(n.h2,{id:"open-dialog-button",children:"Open Dialog Button"}),`
+`,o.jsx(n.p,{children:"Click or tap the button to open a dialog."}),`
+`,o.jsx(e,{of:c})]})}function L(t={}){const{wrapper:n}={...a(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(s,{...t})}):s(t)}export{L as default};
