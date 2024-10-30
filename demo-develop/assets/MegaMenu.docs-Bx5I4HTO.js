c1391bc619dc2aa33b689ab75e39b1bd6aeee153
alimpens <37216945+alimpens@users.noreply.github.com>
2024-10-30 14:51:39 +0000

    Deploying to gh-pages from @ alimpens/design-system@0ab778a0658acb3bb4575dc24347cdea453ad687 🚀

--- demo-develop/assets/MegaMenu.docs-Bx5I4HTO.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as e,ak as m,al as s,an as a}from"./index-D203z5p_.js";import{M as p,a as c}from"./MegaMenu.stories-DYxT1ylt.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CEes6EgT.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-Bi3gjHLv.js";import"./Grid-BN5U4IX6.js";import"./Heading-gp4xEVu7.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-JqIKNsrv.js";import"./index.esm-CahSCGl1.js";import"./Icon-Zdc7Q_73.js";const M=`<!-- @license CC0-1.0 -->
-
-# Mega Menu
-`;function n(o){const r={h2:"h2",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
-`,t.jsx(m,{children:M}),`
-`,t.jsx(s,{}),`
-`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
-`,t.jsx(a,{of:c})]})}function L(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{L as default};
